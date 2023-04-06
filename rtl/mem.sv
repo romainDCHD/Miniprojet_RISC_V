a4cdc5213390d14d249a3dff56ef3de7c590d44a
Winniechas <128793415+Winniechas@users.noreply.github.com>
2023-04-06 16:05:58 +0200

    working

--- rtl/mem.sv
+++ rtl/mem.sv
@@ -1,39 +1,52 @@
-module mem #(parameter  n=20) 
-( 
+//==============================================================================
+//  Filename    : Mem
+//  Description : c'est l'étage de mémoire donnée. On prend addr sur 32 bit pour ne garder que les 8 bits de poids faible. Au départ nous n'initialisons pas la mémoire à 0
+//==============================================================================
+
+module mem #(parameter  n=20) ( 
     input   logic   clk,
     input   logic   rst,
-    input   logic   dmem,
+    input   logic   dmem,	// savoir si ecriture(0) ou lecture(1)
     input   [31:0]  data_w,
     input   [31:0]  addr,
-    output  logic [31:0]  data_r
+    output  logic [31:0]  data_o
   );
+  
     logic   [31:0]  r_mem[n:0];
-    logic   [7:0]  m;
-    logic  [31:0]  data_o;
-    //logic   [8:0]  count = 0;
+    logic  [31:0]  data_r;
+    logic  [7:0]   short_addr;
+    
+    
     always_ff @(posedge clk)
     begin
-        if(rst) begin
-            data_r <=0;
+        if(rst) 
+        begin
+            data_o <= 32'b1;
         end
-        else begin
-            data_r <= data_o;
+        else 
+        begin
+            data_o <= data_r;
         end
     end
     
     always_comb// dmem, data_w, addr
       begin
-        m = addr;
-        if(dmem) begin
-            data_o = r_mem[m];
+      short_addr = addr;
+      	//lecture
+        if(dmem == 1'b1) 
+        begin
+            data_r <= r_mem[ 0 ];
         end
-        else begin
-            r_mem[m] = data_w;
-            data_o = 0;
+        //ecriture
+        else 
+        begin
+            r_mem[ short_addr ] <= data_w;
+            data_r <= 32'b0;
         end
 
     end
 
 
 
+endmodule  
 endmodule    
