686f525ce6fc6f0bcd834211d541b67f82b7cc0f
Winniechas <128793415+Winniechas@users.noreply.github.com>
2023-04-06 13:47:55 +0200

    working

--- rtl/branch_comp.sv
+++ rtl/branch_comp.sv
@@ -1,10 +1,21 @@
+ //==============================================================================
+//  Filename    : branch_comp     
+//  Description : effectue une comparaison entre in_A et in_B (signée ou non, en fonction de BrUn) dont la valeur de retour est donnée par BrEq et BrLT
+//============================================================================== 
+
  module branch_comp (
 	input  logic [31:0]  in_A,
 	input  logic [31:0]  in_B,
-	input  logic [1:0]   BrUn,
+	input  logic   BrUn,
     	output logic   BrEq,
     	output logic   BrLT
 );
+
+logic signed [31:0]  in_A_s;
+logic signed [31:0]  in_B_s;
+assign in_A_s = in_A;
+assign in_B_s = in_B;
+
     	always_comb //in_A, in_B, BrUn
     	begin
     		if(in_A == in_B)
@@ -12,9 +23,9 @@
     				assign BrEq = 1'b1;
     				assign BrLT = 1'b0;
     			end
-    		else if (brUn == 1'b1)
+    		else if (BrUn == 1'b1)
 	    		begin
-	    			if (&signed(in_A) < &signed(in_B))
+	    			if (in_A_s < in_B_s	)
 		    			begin	
 		    				assign BrEq = 1'b0;
 		    				assign BrLT = 1'b1;
